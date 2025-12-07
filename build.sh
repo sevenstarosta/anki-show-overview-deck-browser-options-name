624a98155df6aa3f779d8559bfafbdb1f3bdd01a
sevenstarosta <sevenstarosta@gmail.com>
2025-12-06 19:30:13 -0500

    Include new config files in build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm ./show-overview-deck-browser-options-name.ankiaddon
-zip -r ./show-overview-deck-browser-options-name.ankiaddon README.md LICENSE __init__.py manifest.json
+zip -r ./show-overview-deck-browser-options-name.ankiaddon README.md LICENSE __init__.py manifest.json config.json config.md
